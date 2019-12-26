59de94d1838eaea9eb591d813c6ad05904a1e9ee
Keven Molina <kevenmolina@gmail.com>
2019-12-25 23:31:31 -0600

    created media queries for Iphone 6,7,8,Plus, adjusted veritlce rule with js

--- script/script.js
+++ script/script.js
@@ -2,14 +2,27 @@
 // var x = window.matchMedia
 //get elements for manipulation
 //can width and heigth be set in 
-const window = Window
-const iphone6MQ = window.matchMedia("(max-width: 414px)");
+
+const browser = document.getElementsByName('window')
 const verticalRule = document.querySelector('.v1');
-const mainElem = document.querySelector('main')
+const mainElem = document.querySelector('main');
+const iphone6MQ = window.matchMedia("(max-width: 414px)");
 //function that removes certain elements
 function mobileMarkup(){
-    if(iphone6MQ){
-        mainElem.removeChild(verticalRule)   
+    if(iphone6MQ.matches){
+        console.log("true")
+        verticalRule.className ='v1-hidden'    
+        // mainElem.removeChild(verticalRule)    
+    } else {
+        console.log("false")
+        verticalRule.className ='v1' 
+        //have verticle rule be the 2nd in the array
+        // mainElem.insertBefore(verticalRule, document.querySelector('.application'))
     }
 }
+
+// window.addEventListener('onresize', mobileMarkup);
+//onresize is more effective when it comes to chages in window size
+window.onresize = mobileMarkup;
+window.onload = mobileMarkup;
 
