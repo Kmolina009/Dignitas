e4fbee6c65aae93a67c941efd91493db153e235f
Keven Molina <kevenmolina@gmail.com>
2020-01-04 13:58:52 -0600

    DropDown Menu Added

--- script/script.js
+++ script/script.js
@@ -3,12 +3,18 @@
 //get elements for manipulation
 //can width and heigth be set in 
 
+//Menu Btn elements
 const browser = document.getElementsByName('window')
 const verticalRule = document.querySelector('.v1');
+
+//verticle rule event
 const mainElem = document.querySelector('main');
 const iphone6MQ = window.matchMedia("(max-width: 414px)");
+const menuBtn =document.querySelector('.menu-btn') ;
+const links = document.querySelector('.links');
+
 //function that removes certain elements
-function mobileMarkup(){
+const mobileMarkup =() => {
     if(iphone6MQ.matches){
         console.log("true")
         verticalRule.className ='v1-hidden'    
@@ -21,8 +27,32 @@ function mobileMarkup(){
     }
 }
 
+
+//Menu Bar
+//event name
+
+const menuItems = () =>{
+    console.log("Click")
+    links.classList.toggle('show')
+} 
+menuBtn.addEventListener('click', menuItems)
+
+window.onclick = function(event){
+    if(!event.target.matches(".dropdown")){
+        var menuItems = links
+        // var menuItems = querySelector("links");
+        for(let i = 0; i < links; i++){
+            var openLinks = menuItems[i];
+            if(openLinks.classList.contains('.show')){
+                openLinks.classList.remove('.show')
+            } 
+        }
+    }
+}
+
+//animation to dropdown 
+
 // window.addEventListener('onresize', mobileMarkup);
 //onresize is more effective when it comes to chages in window size
 window.onresize = mobileMarkup;
 window.onload = mobileMarkup;
-
