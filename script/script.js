a975de7bdda545c64cc8dde9ef5e5eff0028823c
Keven Molina <kevenmolina@gmail.com>
2020-01-12 15:02:03 -0600

    Added sticky-header, added babel es6,added mock content

--- script/script.js
+++ script/script.js
@@ -1,40 +1,37 @@
-//Get Window 
-// var x = window.matchMedia
-//get elements for manipulation
-//can width and heigth be set in 
-
-//Menu Btn elements
+// ***Header 
+const header =  document.querySelector('header');
+const sticky = header.offsetTop;
+// const sticky2 = hr.offsetTop;
+// ***Menu Btn elements***
 const browser = document.getElementsByName('window')
 const verticalRule = document.querySelector('.v1');
 
-//verticle rule event
+// ***verticle rule event***
 const mainElem = document.querySelector('main');
 const iphone6MQ = window.matchMedia("(max-width: 414px)");
 const menuBtn =document.querySelector('.menu-btn') ;
 const links = document.querySelector('.links');
-
-//function that removes certain elements
+// ***function that removes certain elements***
 const mobileMarkup =() => {
     if(iphone6MQ.matches){
         console.log("true")
         verticalRule.className ='v1-hidden'    
-        // mainElem.removeChild(verticalRule)    
     } else {
         console.log("false")
         verticalRule.className ='v1' 
-        //have verticle rule be the 2nd in the array
-        // mainElem.insertBefore(verticalRule, document.querySelector('.application'))
     }
 }
 
-
-//Menu Bar
+//Change header menu to a solid shape with an menu as the header, and  the 
+//hamburger button is replace with an X
+// ***Menu Bar***
 //event name
 
 const menuItems = () =>{
     console.log("Click")
     links.classList.toggle('show')
 } 
+
 menuBtn.addEventListener('click', menuItems)
 
 window.onclick = function(event){
@@ -51,8 +48,19 @@ window.onclick = function(event){
 }
 
 //animation to dropdown 
+const  stickyHeader = () => {
+    if(window.pageYOffset > sticky){
+        console.log("Sticky")
+        header.classList.add("sticky");
+        header.classList.add("sticky");
+    }else{
+        console.log("Sticky")
+        header.classList.remove("sticky");
+    }
+};
 
-// window.addEventListener('onresize', mobileMarkup);
-//onresize is more effective when it comes to chages in window size
-window.onresize = mobileMarkup;
+
+// Window Resize
 window.onload = mobileMarkup;
+window.onresize = mobileMarkup;
+window.onscroll = stickyHeader;
